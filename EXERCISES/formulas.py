f56292714c174c493c16bbdd7f6179a64651cd5a
Bridge-jpeg <lyndolusac@gmail.com>
2025-03-31 10:02:05 +0000

    incomplte practice

--- EXERCISES/formulas.py
+++ EXERCISES/formulas.py
@@ -1,3 +1,9 @@
 sample_mean = float(input("number: ")) # mean
 given = float(input("numer: ")) #Xi
+inp_Number = []
+
+def sample_mean(given, n):
+    while inp != "":
+        inp = float(input("Input number: "))
+        inp_Number.append(inp)
 
