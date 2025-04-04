569e51725a0e552190707ab784c95a0807fb2c2b
Bridge-jpeg <lyndolusac@gmail.com>
2025-04-04 14:55:19 +0000

    yessir

--- EXERCISES/formulas.py
+++ EXERCISES/formulas.py
@@ -1,9 +1,21 @@
-sample_mean = float(input("number: ")) # mean
-given = float(input("numer: ")) #Xi
-inp_Number = []
+# sample_mean = float(input("number: ")) # mean
 
-def sample_mean(given, n):
-    while inp != "":
-        inp = float(input("Input number: "))
-        inp_Number.append(inp)
 
+
+def sample_mean(given, no_terms):
+    inp_Number = []
+    while True:
+        inp = input("Input number: ")
+        try:
+            if inp == "":
+                break
+        except:
+            digit = int(inp)
+            inp_Number.append(digit)
+    
+    print(*inp_Number, sep = "\n")
+
+
+given = int(input("numer: ")) #Xi
+no_terms = int(input("terms: "))
+sample_mean(given, no_terms)
