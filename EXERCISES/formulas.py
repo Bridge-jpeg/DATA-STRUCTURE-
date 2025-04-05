9e0488f937b8258294da3457fbdea93ade03fe37
Bridge-jpeg <lyndolusac@gmail.com>
2025-04-05 09:06:20 +0000

    yes

--- EXERCISES/formulas.py
+++ EXERCISES/formulas.py
@@ -1,21 +1,25 @@
-# sample_mean = float(input("number: ")) # mean
-
-
-
-def sample_mean(given, no_terms):
-    inp_Number = []
+def sample_median(given_data):
+    i = 1
+    data_list = []
     while True:
-        inp = input("Input number: ")
-        try:
-            if inp == "":
-                break
-        except:
-            digit = int(inp)
-            inp_Number.append(digit)
-    
-    print(*inp_Number, sep = "\n")
+        if given_data != 69:
+            given_data = int(input("num: "))
+            data_list.append(given_data)
+
+        else:
+            break
+    data_list.sort()
+    if len(data_list) & 2 == 0:
+        middle_median = len(data_list)/2
+        median = ((data_list.index(middle_median - 1) + data_list.index(middle_median + 2)))/2
+        print(median)
+    else: 
+        print("odd")
+
+   
 
 
-given = int(input("numer: ")) #Xi
-no_terms = int(input("terms: "))
-sample_mean(given, no_terms)
+#number_terms = int(input("num: "))
+choice = int(input("1or2: "))
+if choice == 1:
+    sample_median(choice)
---
+++ EXERCISES/test.py
@@ -0,0 +1,10 @@
+test_list = []
+i = 1
+while i == 1:
+    test = int(input("num: "))
+    test_list.append(test)
+    if test == 00:
+        i = 2
+
+formula = len(test_list)/2
+print(test_list[middle])
