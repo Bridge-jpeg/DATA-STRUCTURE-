f58a8cac1fb706afde836b367d00a440174861ce
Bridge-jpeg <lyndolusac@gmail.com>
2025-04-14 23:33:01 +0000

    a large logic

--- EXERCISES/ewan.py
+++ EXERCISES/ewan.py
@@ -2,22 +2,42 @@ list = ['awit', 'rawr', 'g', 't','j']
 amen = list.count('rawr')
 
 print(amen)
-data_list = []
-int_data_list = []
+# data_list = []
+# int_data_list = []
 
-while True:
-    given_data = input("num: ")
-    if given_data != "":
-        data_list.append(given_data)
+# while True:
+#     given_data = input("num: ")
+#     if given_data != "":
+#         data_list.append(given_data)
 
-    else:
-        break
-data_list.sort()
-for item in data_list:
-    int_data_list.append(int(item))
-ls = [type(item) for item in int_data_list]
-print(ls)
+#     else:
+#         break
+# data_list.sort()
+# for item in data_list:
+#     int_data_list.append(int(item))
+# ls = [type(item) for item in int_data_list]
+# print(ls)
 
-print(sum(int_data_list))
+# print(sum(int_data_list))
 
 print(6**2)
+
+my_list = [0.1, 0.0, 0.2, 2.4, 2.7, 4.2]
+leaf = []
+element_to_find = 1
+final_element = element_to_find
+my_list.sort()
+largest_value = my_list[-1]
+
+while my_list:
+    element = my_list[0]  # Always look at the first element
+    if element < element_to_find:
+        leaf.append(element)
+        element_to_find += 1
+        my_list.pop(0)  # Only pop if you're using the value
+        print(leaf)
+        print(my_list)
+    else:
+        # If the current element isn't valid, move it to the end
+        my_list.append(my_list.pop(0))
+
--- EXERCISES/test.py
+++ EXERCISES/test.py
@@ -1,4 +1,4 @@
-stem = [0.1, 0.2, 1.0, 1.2, 2.0, 2.2, 3.3, 3.4, 3.5, 3.8, 3.7]
+stem = []
 leaf = []
 duplicates = []
 
@@ -6,9 +6,36 @@ string_stem = str(stem)
 for item in string_stem:
     duplicates.append(item)
 
-find = duplicates.find("0")
+# find = duplicates.find("0")
 # ls = [type(item) for item in duplicates]
 # print(ls)
-print(find)
+# print(find)
 
 # set()
+
+my_list = [0.1, 0.0, 0.2, 2.4, 2.7, 4.2]
+element_to_find = 1
+index = -1
+
+# for i, element in enumerate(my_list):
+#     if element == element_to_find:
+#         index = i
+#         break
+
+# for i, element in enumerate(my_list):
+#     if element == element_to_find:
+#         index = i
+#         element_to_find = ++1
+# cleared_list = len(my_list) != 0
+my_list = [0.1, 0.0, 0.2, 2.4, 2.7, 4.2]
+leaf = []
+element_to_find = 1
+while my_list:  
+    for element in my_list:
+        if element < element_to_find:
+            leaf.append(element)
+            element_to_find +=1
+            element = my_list.pop(0)
+            print(leaf)
+            print(my_list)
+
